adacf40b35804f2e88ef41ad3ac1036ce8aafcc4
Butler <JBUTLERL@ford.com>
2022-08-24 17:34:09 -0300

    comienzo desarrollo forms

--- app.js
+++ app.js
@@ -10,5 +10,5 @@ app.listen(3030, () => {
 });
 
 app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
+    res.sendFile(path.join(__dirname, 'views/register.html'));
 });
