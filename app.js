8ca7ac38d4e21ad6377b5edef6eab78cd14cc58c
jmacera95 <julimacera@gmail.com>
2022-08-23 23:43:48 -0300

    desarrollo detalle de producto

--- app.js
+++ app.js
@@ -12,3 +12,7 @@ app.listen(3030, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+
+app.get('/product-detail', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productDetail.html'));
+});
