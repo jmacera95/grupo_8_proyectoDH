e1ce01360baf002f68032f3dbf6c8841d113329f
manuelromanf <manuromanfilippelli@gmail.com>
2022-08-29 17:39:13 -0300

    desarrollo de carrito y listado de productos

--- app.js
+++ app.js
@@ -12,3 +12,11 @@ app.listen(3030, () => {
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, 'views/index.html'));
 });
+
+app.get('/products', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productList.html'));
+});
+
+app.get('/cart', (req, res) => {
+    res.sendFile(path.join(__dirname, 'views/productCart.html'));
+});
