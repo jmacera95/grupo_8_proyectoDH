6f964e63fbe5f2293a9998b3c363fcfb46afba2c
jmacera95 <julimacera@gmail.com>
2022-09-05 19:12:05 -0300

    ajustes ruteo

--- src/app.js
+++ src/app.js
@@ -1,24 +1,19 @@
 const path = require('path');
+const mainRoutes = require('./routes/main');
 const productsRoutes = require('./routes/products');
-// const userRoutes = require('./routes/users');
+const userRoutes = require('./routes/users');
+const cartRoutes = require('./routes/cart');
 
 const express = require('express');
 const app = express();
 
 app.use(express.static(path.resolve(__dirname, '../public')));
 
+app.use('/', mainRoutes)
+app.use('/products', productsRoutes);
+app.use('/user', userRoutes);
+app.use('/cart', cartRoutes);
+
 app.listen(3030, () => {
     console.log('Server has started in http://localhost:3030/');
 });
-
-app.get('/', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/index.html'));
-});
-
-app.use('/products', productsRoutes);
-// app.use('/user', userRoutes);
-
-app.get('/cart', (req, res) => {
-    res.sendFile(path.join(__dirname, 'views/productCart.html'))
-});
-
