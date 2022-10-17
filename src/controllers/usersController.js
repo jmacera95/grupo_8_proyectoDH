a64772e1c17f2ea5b86bf14dd25659122016dc9d
manuelromanf <manuromanfilippelli@gmail.com>
2022-10-17 15:24:30 -0300

    avanzando con el user register

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,8 +1,33 @@
 const path = require('path');
+const fs = require('fs');
+const bcrypt = require('bcryptjs');
+
+const userPath = path.join(__dirname, '../database/users.json');
+const readUser = fs.readFileSync(userPath, 'utf-8');
+const users = JSON.parse(readUser);
 
 const usersController = {
     register: (req, res) => {
         res.render('register');
+    } ,
+    postRegister: (req, res) => {
+
+        const password = bcrypt.hashSync(req.body.password, 10)
+
+        const newUser = {
+           id: users.length + 1,
+           fullName: req.body.fullName,
+           email: req.body.email,
+           phone: req.body.phone,
+           CUIT: req.body.cuit,
+           password: password,
+           image: req.file.filename,
+           userType: "basic"
+        }
+        users.push(newUser)
+        const usersJson = JSON.stringify(users, null, 3);
+        fs.writeFileSync(userPath, usersJson);
+        res.redirect('/');
     } ,
     login:(req, res) => {
         res.render('login');
