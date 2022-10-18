4e5be6752134971b926e2e6e856e719d4db3659a
manuelromanf <108841282+manuelromanf@users.noreply.github.com>
2022-10-18 20:38:09 -0300

    Sprint 5 crud usuarios (#26)
    * avanzando con el user register
    
    * avance sprint 5

--- src/routes/users.js
+++ src/routes/users.js
@@ -1,8 +1,22 @@
 const express = require('express');
 const router = express.Router();
+const path = require('path')
 const usersController = require('../controllers/usersController');
+const multer = require('multer')
 
+const storage = multer.diskStorage(
+    {
+        destination: (req, file, callback) => {
+            callback(null, path.join(__dirname, '../../public/images/usersImage'));
+        },
+        filename: (req, file, callback) => {
+            callback(null, `${Date.now()}_${file.originalname}`); // TODO: verificar si este nombre nos sirve o si deberíamos customizarlo más.
+        }
+    }
+);
+const uploadFile = multer({ storage });
 router.get('/register',usersController.register );
+router.post('/register',uploadFile.single('image') , usersController.postRegister );
 
 router.get('/login', usersController.login);
 
