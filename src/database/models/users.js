0c59694d6628f9fa4a736afdbbbe68d9c3056362
jmacera95 <74208045+jmacera95@users.noreply.github.com>
2022-11-21 16:01:34 -0300

    [SPRINT6-CRUD USUARIOS] (#40)
    * crud usuarios
    
    * ajuste password db y string
    
    * seguimos con edit de usuarios
    
    * ajustes con juli
    
    * algunos avances CRUD users
    
    * crud delete de usuarios
    
    * correcion login
    
    * act rama
    
    * boton editar usuario
    
    * JMACERAÂ: debugs CRUD users
    
    * eliminación imagenes no correspondientes
    
    Co-authored-by: manuelromanf <manuromanfilippelli@gmail.com>
    Co-authored-by: Butler <JBUTLERL@ford.com>

---
+++ src/database/models/users.js
@@ -0,0 +1,51 @@
+module.exports = (sequelize, DataTypes) => {
+    const alias = "Users";
+    const cols = {
+        id: {
+            type: DataTypes.INTEGER,
+            primaryKey: true,
+            autoIncrement: true
+        },
+        first_name: {
+            type: DataTypes.STRING(500)
+        },
+        last_name: {
+            type: DataTypes.STRING(500)
+        },
+        legal_identifier: {
+            type: DataTypes.STRING(250)
+        },
+        phone_number: {
+            type: DataTypes.STRING(300)
+        },
+        email: {
+            type: DataTypes.STRING(500)
+        },
+        postal_code: {
+            type: DataTypes.STRING(200)
+        },
+        password: {
+            type: DataTypes.STRING(400)
+        },
+        image_path: {
+            type: DataTypes.STRING(300)
+        },
+        user_type_id: {
+            type: DataTypes.INTEGER
+        }
+
+    }
+    const config = {
+        tableName: "users",
+        timestamps: false
+    }
+    const Users = sequelize.define(alias, cols, config)
+
+    Users.associate = function(models) {
+        Users.belongsTo(models.UserType, {
+            as: "user_type",
+            foreignKey: "user_type_id"
+        })
+    }
+    return Users
+}
