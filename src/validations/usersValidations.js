5b5d74ae9902d32fecef93a7c220dbc303d34c9b
manuelromanf <manuromanfilippelli@gmail.com>
2022-12-11 21:13:45 -0300

    terminando de agregar las validaciones del front para user edit

--- src/validations/usersValidations.js
+++ src/validations/usersValidations.js
@@ -2,9 +2,14 @@ const path = require('path');
 const { body } = require('express-validator');
 
 const usersValidations = {
+
+    registerValidations: [
+        
+    ],
+
     editValidations: [
         body('firstName')
-            // .notEmpty().withMessage('Debes completar el campo Nombre').bail()
+            .notEmpty().withMessage('Debes completar el campo Nombre').bail()
             .isLength({ min: 3 }).withMessage('Revisar la longitud del Nombre').bail()
 
         ,
@@ -14,23 +19,25 @@ const usersValidations = {
         ,
         body('cuit')
             .notEmpty().withMessage('Debes completar el campo CUIT').bail()
+            .isNumeric().withMessage('Debe contener unicamente numeros').bail()
+            .isLength({ min: 11, max: 11 }).withMessage('El CUIT debe contener 11 digitos').bail()
 
         ,
         body('phone')
             .notEmpty().withMessage('Debes completar el campo Telefono').bail()
-
-
+            .isMobilePhone().withMessage('Debe ser un numero de telefono valido').bail()
         ,
         body('email')
             .notEmpty().withMessage('Debes completar el campo Email').bail()
             .isEmail().withMessage('El email no es valido').bail()
-
         ,
         body('cp')
             .notEmpty().withMessage('Debes completar el campo Código Postal').bail()
-
+            .isLength({ min: 4, max: 4 }).withMessage('Debe ser un código postal válido')
         ,
     ]
 }
+
+
 
 module.exports = usersValidations;
