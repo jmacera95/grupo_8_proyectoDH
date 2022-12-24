f89ea7ff86a63b3216c411750733414c6fadda35
Butler <JBUTLERL@ford.com>
2022-12-24 15:21:53 -0300

    api products e image primer version

--- src/controllers/api/productAPIController.js
+++ src/controllers/api/productAPIController.js
@@ -1,33 +1,74 @@
 const db = require('../../database/models');
 const { Op } = require("sequelize");
+const path = require("path");
 
 // const's for business logic related validations
 const currentYear = (new Date).getFullYear();
-const minYearOfManufacture = currentYear-10;
-const maxYearOfManufacture = currentYear-5;
+const minYearOfManufacture = currentYear - 10;
+const maxYearOfManufacture = currentYear - 5;
 
 const productAPIController = {
-    productList: (req, res) => {
-        db.Vehicles.findAll()
-            .then(
-                vehicles => {
-                    let response = {
-                        meta: {
-                            status : 200,
-                            total: vehicles.length,
-                            url: 'api/products'
-                        },
-                        data: vehicles
-                    }
-                        return res.json(response);
-                }
-            )
+
+    getProducts: async (req, res) => {
+        const vehicles = await db.Vehicles.findAll({
+            include: [{
+                model: db.VehiclesModels,
+                as: "vehicles_models"
+            }],
+            attributes: ["id"],
+        })
+        const unDuenio = await db.Vehicles.findAll({
+            where: {
+                total_owners: 1
+            }
+        });
+
+        const dosDuenios = await db.Vehicles.findAll({
+            where: {
+                total_owners: 2
+            }
+        });
+
+        vehicles.map((vehicle) => (vehicle.dataValues.name = vehicle.vehicles_models.model_name));
+        vehicles.map((vehicle) => (vehicle.dataValues.description = vehicle.vehicles_models.name));
+        vehicles.map((vehicle) => (vehicle.dataValues.detail = `/api/products/${vehicle.id}`));
+
+        const response = {
+            count: vehicles.length,
+            count_by_category: {
+                un_duenio: unDuenio.length,
+                dos_duenios: dosDuenios.length
+            },
+            vehicles: vehicles,
+
+        };
+        return res.json(response);
     },
+
+    getProduct: async (req, res) => {
+
+        
+
+    },
+
+    getProductImage: async (req, res) => {
+        const productImagePath = await db.Vehicles.findByPk(req.params.id, {
+          attributes: ["image_path"],
+        });
+        res.set({ "Content-Type": "image/png" });
+        return res.sendFile(
+          path.resolve(
+            __dirname,
+            `../../../public/images/products/${productImagePath.image_path}`
+          )
+        );
+      },
+
     activeVehicleModelsList: (req, res) => {
         db.VehiclesModels.findAll(
             {
                 where: {
-                    year: {[Op.gte] : minYearOfManufacture, [Op.lte] : maxYearOfManufacture}
+                    year: { [Op.gte]: minYearOfManufacture, [Op.lte]: maxYearOfManufacture }
                 }
             }
         )
@@ -35,13 +76,13 @@ const productAPIController = {
                 vehicles => {
                     let response = {
                         meta: {
-                            status : 200,
+                            status: 200,
                             total: vehicles.length,
                             url: 'api/products/vehicles_models/active'
                         },
                         data: vehicles
                     }
-                        return res.json(response);
+                    return res.json(response);
                 }
             )
     }
--- src/routes/api/products.js
+++ src/routes/api/products.js
@@ -2,7 +2,9 @@ const express = require('express');
 const router = express.Router();
 const productAPIController = require('../../controllers/api/productAPIController');
 
-router.get('/', productAPIController.productList);
+router.get('/', productAPIController.getProducts);
+router.get('/:id', productAPIController.getProduct);
+router.get('/:id/image', productAPIController.getProductImage);
 router.get('/vehicles_models/active', productAPIController.activeVehicleModelsList);
 
 module.exports = router;
