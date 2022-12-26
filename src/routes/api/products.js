241cc368e7ef9bb761971d117c48a05d59765241
Juan Butler <juanbutlerl94@gmail.com>
2022-12-26 19:33:11 -0300

    Jbutlerl sprint8 (#60)
    * api products e image primer version
    
    * correccion de api para validacion de productos
    
    Co-authored-by: Butler <JBUTLERL@ford.com>

--- src/routes/api/products.js
+++ src/routes/api/products.js
@@ -2,7 +2,11 @@ const express = require('express');
 const router = express.Router();
 const productAPIController = require('../../controllers/api/productAPIController');
 
-router.get('/', productAPIController.productList);
+router.get('/', productAPIController.getProducts);
+router.get('/:id', productAPIController.getProduct);
+router.get('/:id/image', productAPIController.getProductImage);
+
+router.get('/validations/detalle', productAPIController.validationsProductList);
 router.get('/vehicles_models/active', productAPIController.activeVehicleModelsList);
 
 module.exports = router;
